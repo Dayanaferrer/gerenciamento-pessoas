1d6cb2c5a6514556a33648b879491aed3e6656b8
Dayanaferrer <dayanaferreira2@gmail.com>
2024-05-14 14:42:54 -0300

    Adicionando metodos em repository para serem usados posteriormente

--- src/main/java/com/Attus/pessoas/repositories/EnderecoRepository.java
+++ src/main/java/com/Attus/pessoas/repositories/EnderecoRepository.java
@@ -1,5 +1,7 @@
 package com.Attus.pessoas.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.Attus.pessoas.models.EnderecoModel;
 
 @Repository
 public interface EnderecoRepository extends JpaRepository<EnderecoModel, Long> {
+	 List<EnderecoModel> findByPrincipal(boolean principal);
 
 }
--- src/main/java/com/Attus/pessoas/repositories/PessoaRepository.java
+++ src/main/java/com/Attus/pessoas/repositories/PessoaRepository.java
@@ -1,5 +1,8 @@
 package com.Attus.pessoas.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,7 @@ import com.Attus.pessoas.models.PessoaModel;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<PessoaModel, Long> {
+	List<PessoaModel> findByNomeCompleto(String nomeCompleto);
+    Optional<PessoaModel> findById(Long id);
 
 }
